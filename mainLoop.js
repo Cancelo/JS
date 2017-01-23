541566a27d9f5ac064875d586eef6324ba5ab88a
Cancelo <Cancelo@users.noreply.github.com>
2017-01-23 17:17:51 +0100

    Add files via upload

--- mainLoop.js
+++ mainLoop.js
@@ -1,20 +1,30 @@
+//ls = 1000ms
+
 var mainLoop = {
 	idEjecucion: null,
 	ultimoRegistro: 0,
 	aps: 0,
 	fps: 0,
 	start: function(registroTemporal) {
-		mainLoop.idEjecucion = window.requestAnimationFrame(mainLoop.iterar);
+		mainLoop.idEjecucion = window.requestAnimationFrame(mainLoop.start);
 
-		mainLoop.refresh();
+		mainLoop.refresh(registroTemporal);
+		mainLoop.draw();
+
+		if(registroTemporal - mainLoop.ultimoRegistro > 999) {
+			mainLoop.ultimoRegistro = registroTemporal;
+			console.log("APS: " + mainLoop.aps + " | FPS: " + mainLoop.fps);
+			mainLoop.aps = 0;
+			mainLoop.fps = 0;
+		}
 	},
 	stop: function() {
 
 	},
 	refresh: function(registroTemporal) {
-
+		mainLoop.aps++;
 	},
 	draw: function(registroTemporal) {
-
+		mainLoop.fps++;
 	}
 }
